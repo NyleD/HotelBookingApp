a4bdc51a23f2f062d997dc134fb9643f2074946c
Nyle Kabiruddin Dharani <nkdharani@uwaterloo.ca>
2020-04-10 18:56:07 -0400

    new models

--- hotels/models.py
+++ hotels/models.py
@@ -3,23 +3,22 @@ from django.db.models import SET_NULL
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Customer(models.Model):
-    user_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=50, default="")
+    email = models.CharField(max_length=50, null=True)
     
 class Room(models.Model):
-    room_num = models.IntegerField(unique=True)
     beds = models.IntegerField(default=1, validators=[MaxValueValidator(3), MinValueValidator(1)])
     view = models.CharField(max_length=50, default="None")
     luxury = models.CharField(max_length=50, default="None")
 
 class Booking(models.Model):
-    book_id = models.IntegerField(unique=True)
     customer = models.ForeignKey(Customer, related_name='bookings', null=True, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, related_name='bookings', null=True, on_delete=models.CASCADE)
     numguests = models.IntegerField(default=1)
     cancelled = models.BooleanField(default=False)
     checkin = models.DateField(auto_now=False, null=True)
     checkout = models.DateField(auto_now=False, null=True)
+    rating = models.IntegerField(default=5)
 
 
 
