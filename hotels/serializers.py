97312d65beaafcc7fc8074689aa1fc068a0d0aff
KomalSarvani <mkomalsarvani@gmail.com>
2020-04-12 14:00:46 -0400

    updated serializers to show ids

--- hotels/serializers.py
+++ hotels/serializers.py
@@ -4,15 +4,15 @@ from .models import Customer, Room, Booking
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ('name', 'email')
+        fields = ('id', 'name', 'email')
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ('beds', 'view', 'luxury')
+        fields = ('id', 'beds', 'view', 'luxury')
 
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = ('customer', 'room', 'numguests', 'cancelled', 'checkin', 'checkout', 'rating')
+        fields = ('id', 'customer', 'room', 'numguests', 'cancelled', 'checkin', 'checkout', 'rating')
