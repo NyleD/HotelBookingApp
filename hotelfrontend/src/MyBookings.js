11a1df6fcce72a6f27b9a2830c1bccb189b2fa26
Jacky Chen <jy32chen@uwaterloo.ca>
2020-04-14 15:29:45 -0400

    Small bugfix in mybookings

--- hotelfrontend/src/MyBookings.js
+++ hotelfrontend/src/MyBookings.js
@@ -48,7 +48,7 @@ export class MyBookings extends React.Component {
 	};
 
 	fetchData() {
-		this.fetchBookings()
+		this.fetchBookings(false)
 			.then(() => { this.fetchRooms(); })
 			.then(() => {
 				this.setState({
@@ -57,14 +57,15 @@ export class MyBookings extends React.Component {
 			});
 		};
 
-	fetchBookings() {
-		return fetch(urlBookings+"?customer="+this.state.newCustomer)
+	fetchBookings(isUpdated) {
+		return fetch(urlBookings+"?customer="+ (isUpdated ? this.state.customer : this.state.newCustomer))
 			.then((response) => { return response.json(); })
 			.then((data) => {
 				this.setState({
 					bookingData: data,
 					isDialogOpen: false,
-					customer: this.state.newCustomer,
+					customer: isUpdated ? this.state.customer : this.state.newCustomer,
+					newCustomer: isUpdated ? this.state.customer : this.state.newCustomer,
 					bookingError: false,
 				});
 			});
@@ -105,7 +106,7 @@ export class MyBookings extends React.Component {
 		};
 
 		const refreshBookings = (event) => {
-			this.fetchBookings();
+			this.fetchBookings(false);
 		};
 
 		const toggleDisplay = (event) => {
@@ -188,7 +189,7 @@ export class MyBookings extends React.Component {
 				if (data.result == "Fail") {
 					this.openErrorSnackbar();
 				} else if (data.result == "Success") {
-					this.fetchBookings();
+					this.fetchBookings(true);
 				}
 			});
 		};
@@ -296,7 +297,7 @@ export class MyBookings extends React.Component {
 				if (data.result == "Fail") {
 					this.openErrorSnackbar();
 				} else if (data.result == "Success") {
-					this.fetchBookings();
+					this.fetchBookings(true);
 				}
 			});
 		};
@@ -322,7 +323,7 @@ export class MyBookings extends React.Component {
 				if (data.result == "Fail") {
 					this.openErrorSnackbar();
 				} else if (data.result == "Success") {
-					this.fetchBookings();
+					this.fetchBookings(true);
 				}
 			});
 		};
